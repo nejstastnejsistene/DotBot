53a4e85487a37d9ecb2df3a4389c47374cbe97da
Peter Johnson <pajohnson@email.wm.edu>
2014-02-15 14:41:57 -0500

    heroku+flask+twilio

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dotbot.mms:app
---
+++ dotbot/mms.py
@@ -0,0 +1,12 @@
+import twilio.twiml
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/twilio', methods=['POST'])
+def twilio_endpoint():
+    r = twilio.twiml.Response()
+    r.message(str(request.form))
+    return r
+
+if __name__ == '__main__':
+    app.run(debug=True)
