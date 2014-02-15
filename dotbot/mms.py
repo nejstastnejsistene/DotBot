d82d1733a014ec04e60d60085fa33df3ee229a58
Peter Johnson <pajohnson@email.wm.edu>
2014-02-15 15:31:53 -0500

    See if reading an image from twilio works.

--- dotbot/mms.py
+++ dotbot/mms.py
@@ -1,11 +1,21 @@
+from screenreader import read_game_screen
+from ai import smart_path
+
+import tempfile
+import requests
 import twilio.twiml
 from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/twilio', methods=['POST'])
 def twilio_endpoint():
+    img = requests.get(request.form['MediaUrl0'])
+    with open('screenshot.png') as f:
+        f.write(img.text)
+    info = read_game_screen('screenshot.png')
+    _, path = smart_path(info)
     r = twilio.twiml.Response()
-    r.message(str(request.form))
+    r.message(str(path))
     return str(r)
 
 if __name__ == '__main__':
