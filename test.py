920d2eb42169e919f8545b694bf51214bfac508d
root <root@dropbot.peterjohnson.io>
2014-02-16 02:42:41 +0000

    bad

--- test.py
+++ test.py
@@ -4,14 +4,13 @@ length_of_game = 35
 if __name__ == '__main__':
     from dotbot.ai import *
     import dotbot.fakedots
-    colors = [[random.randrange(6) for i in range(6)] for r in range(6)]
-    #print colors
-    for i in range(length_of_game):
-        w, path = smart_path(colors) #
-        #print w, path
-        colors = dotbot.fakedots.play_path(colors, path)
-        #print "move made"
+    for i in range(10):
+        colors = [[random.randrange(6) for i in range(6)] for r in range(6)]
+        for i in range(length_of_game):
+            w, path = smart_path(colors) 
+            colors = dotbot.fakedots.play_path(colors, path)
+        
 
-    print dotbot.fakedots.totes_score
-
+        print dotbot.fakedots.totes_score
+        dotbot.fakedots.totes_score = 0
  
