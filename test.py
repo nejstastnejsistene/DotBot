080a0c9e88abdaddb21698a8da9a5e94577d3ec5
root <root@dropbot.peterjohnson.io>
2014-02-16 05:40:34 +0000

    why didnt test push

--- test.py
+++ test.py
@@ -6,17 +6,22 @@ if __name__ == '__main__':
     import dotbot.fakedots
     colors = [[random.randrange(5) for i in range(6)] for r in range(6)]
     #print colors
-    for j in range(25):
+    complete = 0
+    while complete < 10:
+        print "completed:", complete
         for i in range(length_of_game):
             w, path = smart_path(colors) #
             #print w, path
             colors = dotbot.fakedots.play_path(colors, path)
-            #print "move made"
+            print "move made"
             if dotbot.fakedots.totes_score < 5 or (i>5 and dotbot.fakedots.totes_score/i < 10 ): 
                 dotbot.fakedots.totes_score = 0
+                print "quitting early"
+                complete -= 1
                 break
 
         print dotbot.fakedots.totes_score
         dotbot.fakedots.totes_score = 0
+        complete += 1
 
  
