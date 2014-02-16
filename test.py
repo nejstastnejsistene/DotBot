f9020af741b69a36fa6b0c669ad61c675ea49a9c
Nathan <ncschaaf@email.wm.edu>
2014-02-16 00:59:44 -0500

    various weights for testing

--- test.py
+++ test.py
@@ -7,21 +7,22 @@ if __name__ == '__main__':
     colors = [[random.randrange(5) for i in range(6)] for r in range(6)]
     #print colors
     complete = 0
-    while complete < 10:
-        print "completed:", complete
-        for i in range(length_of_game):
-            w, path = smart_path(colors) #
-            #print w, path
-            colors = dotbot.fakedots.play_path(colors, path)
-            print "move made"
-            if dotbot.fakedots.totes_score < 5 or (i>5 and dotbot.fakedots.totes_score/i < 10 ): 
-                dotbot.fakedots.totes_score = 0
-                print "quitting early"
-                complete -= 1
-                break
+    while complete < 50:
+        for weight in (1.1, 1.25, 1.5, 1.75, 2):
+            print "completed:", complete
+            for i in range(length_of_game):
+                w, path = smart_path(colors) #
+                #print w, path
+                colors = dotbot.fakedots.play_path(colors, path)
+                print "move made"
+                if dotbot.fakedots.totes_score < 5 or (i>5 and dotbot.fakedots.totes_score/i < 10 ): 
+                    dotbot.fakedots.totes_score = 0
+                    print "quitting early"
+                    complete -= 1
+                    break
 
-        print dotbot.fakedots.totes_score
-        dotbot.fakedots.totes_score = 0
-        complete += 1
+            print weight, ':', dotbot.fakedots.totes_score
+            dotbot.fakedots.totes_score = 0
+            complete += 1
 
  
