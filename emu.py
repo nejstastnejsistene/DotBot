81f70370d12c948698e3c5dd7ba1695ff7f0df9c
Peter Johnson <pajohnson@email.wm.edu>
2014-02-15 23:04:06 -0500

    Implemented moves to mutate the grid.

--- emu.py
+++ emu.py
@@ -14,6 +14,42 @@ class DotsGrid(object):
 
     def __init__(self, colors=None):
         self.colors = colors or [random.randrange(5) for i in range(36)]
+        self.score = 0
+
+    def __getitem__(self, rc):
+        row, col = rc
+        return self.colors[6 * row + col]
+
+    def __setitem__(self, rc, color):
+        row, col = rc
+        self.colors[6 * row + col] = color
+
+    def apply_path(self, path):
+        unique_coords = set(path)
+        
+        if len(path) != len(unique_coords):
+            # It formed a square, clear all dots.
+            self.expand(*path[0])
+        else:
+            # Otherwise just remove the dots in the path.
+            # Sort them by row first.
+            dots = sorted(unique_coords, key=lambda x: x[0])
+            for row, col in dots:
+                self.shrink(row, col)
+
+    def shrink(self, row, col, exclude=None):
+        self.score += 1
+        for r in range(row, 0, -1):
+            self[r, col] = self[r - 1, col] 
+        choices = filter(lambda x: x != exclude, range(5))
+        self[0, col] = random.choice(choices)
+
+    def expand(self, row, col):
+        color = self[row, col]
+        for row in range(6):
+            for col in range(6):
+                if self[row, col] == color:
+                    self.shrink(row, col, exclude=color)
 
     def __repr__(self):
         s = StringIO()
@@ -27,4 +63,15 @@ class DotsGrid(object):
 
 
 if __name__ == '__main__':
-    print DotsGrid()
+
+    grid = [4, 2, 0, 2, 1, 0, \
+            3, 2, 1, 4, 4, 4, \
+            0, 3, 2, 4, 2, 0, \
+            4, 0, 3, 2, 3, 3, \
+            3, 3, 1, 4, 2, 1, \
+            3, 1, 4, 2, 1, 4]
+
+    emu = DotsGrid(grid)
+    print emu
+    emu.apply_path(((5, 0), (4, 0), (4, 1)))
+    print emu
