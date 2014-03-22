cb747eb4878902b6e11e806c091539b12caac1df
Peter Johnson <pajohnson@email.wm.edu>
2014-03-22 11:54:58 -0400

    Added loop to continuously play dots in infinite mode or something.

---
+++ loop.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ip=$1
+
+connect() {
+    output=`adb devices | grep $ip`
+    if [ -z "$output" ]; then
+        echo 'connecting...'
+        adb kill-server
+        adb start-server
+        adb connect $ip
+    fi
+}
+
+connect
+while true; do
+    ./main.py || connect
+    sleep 0.5
+done
